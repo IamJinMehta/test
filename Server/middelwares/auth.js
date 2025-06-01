7a54b9bf372bc9659cf3c9a1d3b67e3ac2890f2c
iamjinmehta <jinendramehta2321@gmail.com>
2025-06-01 15:31:26 +0530

    FIX: auth middleware change.

--- Server/middelwares/auth.js
+++ Server/middelwares/auth.js
@@ -1,18 +1,21 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-    const token = req.headers.authorization;
+  let token = req.headers.authorization;
 
-    if (!token) {
-        res.status(401).json({ message: "Authentication failed , Token missing" });
-    }
-    try {
-        const decode = jwt.verify(token, 'secret_key')
-        req.user = decode
-        next();
-    } catch (err) {
-        res.status(500).json({ message: 'Authentication failed. Invalid token.' })
-    }
-}
+  if (!token) {
+    res.status(401).json({ message: "Authentication failed , Token missing" });
+  } else if (token && token.startsWith("Bearer ")) {
+    token = token.slice(7, token.length);
+  }
 
-module.exports = auth
+  try {
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decode;
+    next();
+  } catch (err) {
+    res.status(500).json({ message: "Authentication failed. Invalid token." });
+  }
+};
+
+module.exports = auth;
